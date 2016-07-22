37721337b2d2bc0e7269d98c26db98650b34c1b3
Denis Gvozd <dgvozd@dev-team.com>
2016-07-22 23:13:45 +0300

    user context package

---
+++ UserContext.cs
@@ -0,0 +1,23 @@
+﻿using System.Security.Claims;
+using System.Web;
+using Microsoft.AspNet.Identity;
+
+namespace DevTeam.UserContext
+{
+    public static partial class UserContext
+    {
+        public static bool IsAuthenticated => HttpContext.Current.User.Identity.IsAuthenticated;
+
+        public static ClaimsIdentity User => HttpContext.Current.User.Identity as ClaimsIdentity;
+
+        public static int UserId
+        {
+            get
+            {
+                var userId = 0;
+                int.TryParse(User.GetUserId(), out userId);
+                return userId;
+            }
+        }
+    }
+}
